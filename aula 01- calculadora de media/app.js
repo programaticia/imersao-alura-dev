074cf908607feaf7392b3b70cab3232eb8057596
Leticia Almeida <programaticia@gmail.com>
2022-03-10 00:45:25 -0300

    inicio calculadora de médias

---
+++ aula 01- calculadora de media/app.js
@@ -0,0 +1,18 @@
+var nome = "Leticia"
+
+var notaDoPrimeiroBimestre = 9.43
+var notaDoSegundoBimestre = 7.23
+var notaDoTerceiroBimestre = 8.23
+var notaDoQuartoBimestre = 9.18
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo(a) " + nome)
+console.log(notaFixada)
+
+//Revisão
+//variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
+
+//iconcatenação = junção de uma string com uma variável ex.:("Bem vindo(a) " + nome)
