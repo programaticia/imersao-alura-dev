aacd02920e66947cc39275445dfad565aaa92a93
Leticia Almeida <programaticia@gmail.com>
2022-03-11 14:49:52 -0300

    correção aula 01

--- aula 01- calculadora de media/app.js
+++ aula 01- calculadora de media/app.js
@@ -15,4 +15,29 @@ console.log(notaFixada)
 //Revisão
 //variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
 
-//iconcatenação = junção de uma string com uma variável ex.:("Bem vindo(a) " + nome)
+//concatenação = junção de uma string com uma variável ex.:("Bem vindo(a) " + nome)
+
+function media(){
+    var valorElemento1 = document.getElementById("primeiroBimestre");
+    var primeiroBimestre = valorElemento1.value;
+    var valorPrimeiroBimestre = parseFloat(primeiroBimestre);
+
+    var valorElemento2 = document.getElementById("segundoBimestre");
+    var segundoBimestre = valorElemento2.value;
+    var valorSegundoBimestre = parseFloat(segundoBimestre);
+
+    var valorElemento3 = document.getElementById("terceiroBimestre");
+    var terceiroBimestre = valorElemento3.value;
+    var valorTerceiroBimestre = parseFloat(terceiroBimestre);
+
+    var valorElemento4 = document.getElementById("quartoBimestre");
+    var quartoBimestre = valorElemento4.value;
+    var valorQuartoBimestre = parseFloat(quartoBimestre);
+
+    var valorMedia = (valorPrimeiroBimestre + valorSegundoBimestre + valorTerceiroBimestre + valorQuartoBimestre) / 4;
+    console.log(valorMedia);
+
+    var elementoValorCalculado = document.getElementById("resultado");
+    var resultado = "A sua média esse ano foi de " + valorMedia;
+    elementoValorCalculado.innerHTML = resultado;
+}
