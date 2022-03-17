66e97993d972409b15dbd83e51d5376de90739b3
Leticia Almeida <programaticia@gmail.com>
2022-03-17 19:27:39 -0300

    adicionando mais filmes

--- aula 04 - array, lista e aluraflix/app.js
+++ aula 04 - array, lista e aluraflix/app.js
@@ -2,7 +2,15 @@ var listaFilmes = [
   "https://image.tmdb.org/t/p/w342/t1G9OlLHGTPRpnFnB6NHAlOqmHI.jpg",
   "https://cdn.europosters.eu/image/750/posters/interstellar-go-further-i23316.jpg",
   "https://occ-0-999-1001.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABZhtiNpCsurcjI-GFqq6Uijb78Kkho-z_5oJorh6pvGXty38yJloCOz5lF1WT5R35vmC3CiNQ57oNiFO7Oayh_QaPnN1.jpg",
-  "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg"
+  "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg",
+  "https://www.themoviedb.org/t/p/original/fptnZJbLzKUHeNlYrAynbyoL5YJ.jpg",
+  "https://pics.filmaffinity.com/Gunpowder_Milkshake-801891929-large.jpg",
+  "https://i.pinimg.com/originals/89/de/72/89de72d199eb933e2f6f0dc0ff530ed3.png",
+  "https://lumiere-a.akamaihd.net/v1/images/image_7b3ee600.jpeg",
+  "https://www.gstatic.com/tv/thumb/v22vodart/15839539/p15839539_v_v8_ab.jpg",
+  "https://assets0.minhaserie.com.br/uploads/editor_pictures/000/049/472/content_pic.jpg",
+  "https://upload.wikimedia.org/wikipedia/pt/d/db/Brave_Teaser_Poster.jpg",
+  "https://uauposters.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/2/926020211216-uau-posters-brilho-eterno-de-uma-mente-sem-lembrancas-eternal-sunshine-of-the-spotless-mind-filmes-3.jpg"
 ];
 
 for (var i = 0; i < listaFilmes.length; i++) {
